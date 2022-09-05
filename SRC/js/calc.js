0ab2b9d4971400728f70d5a5cdcc9e04257f15d9
SshNazarov <110193042+SshNazarov@users.noreply.github.com>
2022-09-05 13:17:37 +0300

    Add files via upload

--- SRC/js/calc.js
+++ SRC/js/calc.js
@@ -1,53 +1,68 @@
+$(document).ready(function(){
+    
+    let options = {threshold: [0.4]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.animat');
+    elements.each((i,el) => {
+        observer.observe(el);
+    });
 
-let site, design, adaptive;
-
-let calculator = {
-    price: [
-        [100, 300, 500],
-        [120, 240, 300],
-        [130, 160, 200],
-    ],
-    days: [
-        [2, 3, 5],
-        [3, 4, 5],
-        [1, 2, 3],
-    ],
-    run(siteType, designType, adaptiveType){
-        let sum = parseInt(calculator.price[0][siteType-1]) + parseInt(calculator.price[1][designType-1]) + parseInt(calculator.price[2][adaptiveType-1]);
-        let days = parseInt(calculator.days[0][siteType-1]) + parseInt(calculator.days[1][designType-1]) + parseInt(calculator.days[2][adaptiveType-1]);
-        alert("Сроки: "+ days + "Стоимость: "+ sum);
+    function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('show-animation');
+        }
+    });
+}
+    
+   $(".preloader").css("display", "none");
+    
+    function calculate(){
+     let sum = parseInt($("#select1 option:selected").val()) + parseInt ($("#select2 option:selected").val()) + parseInt($("#select3 option:selected").val());
+     let days = parseInt($("#select1 option:selected").attr("days")) + parseInt ($("#select2 option:selected").attr("days")) + parseInt($("#select3 option:selected").attr("days"));     
+    $(".days .digit").text(days);
+    $(".price .digit").text(sum); 
     }
-};
-
-function getAnswers(){
-        site = prompt("Какой тип сайта вы хотите? 1 - Сайт-визитка 2 - Корпаративный сайт 3 - Интернет-магазин");
-    if(site != 1 && site != 2 && site != 3 ){
-        alert("Такого варианта нет");
-        getAnswers();
-        return;
-    };
-        design = prompt("Какой тип дизайна вы хотите? 1 - Шаблонный 2 - Уникальный 3 - WOW-дизайн");
-    if(design != 1 && design != 2 && design != 3 ){
-        alert("Такого варианта нет");
-        getAnswers();
-        return;
-    };
-        adaptive = prompt("Какой тип адаптивности вы хотите? 1 - Только ПК 2 - Только-мобильный сайт 3 - ПК + Мобильный");
-    if(adaptive != 1 && adaptive != 2 && adaptive != 3 ){
-        alert("Такого варианта нет");
-        getAnswers();
-        return;
-    };
-    calculator.run(site, design, adaptive);
-};
-
-getAnswers();
-
-
-
-
-
-
-
-
+    $("select").on("change", function(){
+       calculate();
+    });
+    calculate();
+    
+    
+    
+    $(window).scroll(() => {
+        let scrollDistance = $(window).scrollTop();
+        
+        $('.section').each((i, el) => {
+            
+            if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
+                $('nav a').each((i, el) => {
+                    if ($(el).hasClass('active')) {
+                        $(el).removeClass('active');
+                    }
+                });
+                $('nav li:eq(' + i + ')').find('a').addClass('active');
+            }
+        });
+    });
+    
+        $('.image-link').magnificPopup({type:'image'});
+   /*     let options = {threshold: [0.4]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.animat');
+    elements.each((i,el) => {
+        observer.observe(el);
+    });
+        function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.src = change.target.dataset.src;
+        }
+    });
+    };*/
+    
+  
+    
+});
+    
 
